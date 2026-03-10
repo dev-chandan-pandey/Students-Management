1d884a6c5a0da428c70c1a975c7bef5b1f73bc03
dev-chandan-pandey <iamchandanpandey11@gmail.com>
2026-03-10 19:39:13 +0530

    backend live

--- students-table-app/src/utils/api.ts
+++ students-table-app/src/utils/api.ts
@@ -1,7 +1,14 @@
+// import axios from "axios"
+
+// const API = axios.create({
+//   baseURL: "http://localhost:3000",
+// })
+
+// export default API
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://students-management-44hd.onrender.com",
 })
 
 export default API
